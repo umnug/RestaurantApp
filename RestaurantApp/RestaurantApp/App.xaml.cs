77a61e3fdbd3938425b89a33b337bf150181fb01
DESKTOP-5K50S1Q\johnc <john.c.harris@live.com>
2017-01-14 17:59:52 -0700

    Add project files.

---
+++ RestaurantApp/RestaurantApp/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using FreshMvvm;
+using RestaurantApp.PageModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace RestaurantApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var tabContainer = new FreshTabbedNavigationContainer();
+
+            tabContainer.AddTab<ContactPageModel>("Contact", "");
+            tabContainer.AddTab<GalleryPageModel>("Gallery", "");
+            tabContainer.AddTab<MapPageModel>("Map", "");
+            tabContainer.AddTab<MenuPageModel>("Menu", "");
+            tabContainer.AddTab<SpecialOffersPageModel>("Special Offers", "");
+
+            MainPage = tabContainer;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ RestaurantApp/RestaurantApp/PageModels/ContactPageModel.cs
@@ -0,0 +1,22 @@
+﻿using FreshMvvm;
+using Plugin.Messaging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RestaurantApp.PageModels
+{
+    class ContactPageModel : FreshBasePageModel
+    {
+        public Command CallCommand { get { return new Command(Call); } }
+
+        private void Call(object obj)
+        {
+            if (CrossMessaging.Current.PhoneDialer.CanMakePhoneCall)
+                CrossMessaging.Current.PhoneDialer.MakePhoneCall("555-555-5555");
+        }
+    }
+}
